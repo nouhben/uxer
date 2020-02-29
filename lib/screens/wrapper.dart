c744051b4710137a833f9c506e6a3bb5f7eccd73
nouh ben kadi <benkadi.nouh@icloud.com>
2020-02-29 20:57:12 +0100

    initial commit : structure of the app is done --> next ep4

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_uxer_app/screens/home/home.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Home(); //home or authenticate widget
+  }
+}
